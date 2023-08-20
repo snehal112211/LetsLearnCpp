1976d2ef1dcdf03d522f62a17893670f2a03603a
Snehal Patil <141567039+snehal112211@users.noreply.github.com>
2023-08-21 00:23:27 +0530

    Add files via upload

---
+++ Basic.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+// this line indicate that there is the 
+//iostream file so you can use the content of that file also
+// It is called as the header file 
+// io means that input and output
+// It helps as in taking input and output
+//using namespace std;
+int main(){
+// here int means integer
+// main is the function
+// here the entry of the function happens
+// there are many function in the main
+// called as the entry point
+   std::cout<<"Hello world!!";
+   return 0;
+// can also write the std as give up or down
+// return 0 successful termination of the programe
+}
+
+// Using Vs Code as the beginning
