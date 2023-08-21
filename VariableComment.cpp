5e8ae034cbee209c37bb229c563197fb0c962b38
Snehal Patil <141567039+snehal112211@users.noreply.github.com>
2023-08-22 00:27:17 +0530

    Add files via upload

---
+++ VariableComment.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+// This program is created by Snehal Patil
+// This is callled as the comment
+/*
+this
+is 
+the 
+multiline 
+comment
+*/
+int main(){
+    int sum = 6;
+    // this is the Variable
+    cout<< "Hello World "<<sum;
+    return 0;
+}
