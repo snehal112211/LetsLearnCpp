20b64e626c03a64355563fa58553dd0c1a2cf530
Snehal Patil <141567039+snehal112211@users.noreply.github.com>
2023-08-24 00:36:40 +0530

    Add files via upload

---
+++ 4-VariableScopeDataType.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // int a= 34;
+    // int b= 45;
+    int a = 23, b = 34;
+    float te = 3.45;
+    char c ='s';
+    cout<<"This is the 4th Tutorial.\nHere the value of the a is " << a<<".\nThe value of the b is "<<b<<".\n";
+    cout<<"The value of the te is "<<te<<".\n";
+    cout<<"the value of the c is "<<c;
+    return 0;
+}
+
+// for char we have to use only one character
