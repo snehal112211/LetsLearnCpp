d5fcfe781c51addda8e6aba6c7d7c8f0ae66df4c
Snehal Patil <141567039+snehal112211@users.noreply.github.com>
2023-08-24 22:38:25 +0530

    Add files via upload

---
+++ 5-InputOutput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int num1, num2;
+   cout<< "Enter the value of num1 ";/* this is called as the insertion operator*/
+   cin>>num1;/*"This is the extraction oprerator"*/
+
+   cout<<"Enter the value of the num2 ";
+   cin>>num2;
+
+   cout<<"the sum of num1 and num2 is: "<<num1+num2;
+   
+   return 0; 
+}
+
